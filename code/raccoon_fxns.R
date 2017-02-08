3c35fc1d6c17b3bb94f8e2173d78ab89758137da
Mark Wilber <mqwilber@gmail.com>
2017-02-08 08:38:36 -0800

    Cleaned up parameters from Issue #33

--- code/raccoon_fxns.R
+++ code/raccoon_fxns.R
@@ -106,6 +106,7 @@ get_cull_indices = function(cull_params, raccoon_dead_alive_vect,
     #   - Additional parameters: `quota`: Number of raccoons to kill per time step
     # 2. `age`: Only cull juveniles i.e. less than a year old
     #   - Additional parameters: `quota`: Number of raccoons to kill per time step
+    #   -                        `age`: age in months below which you cull raccoons
     # 3. `human`: Only cull individuals with a human overlap greater than 
     #              `overlap_threshold`
     #   - Additional parameters: `quota`: absolute number of raccoons to kill
@@ -140,7 +141,7 @@ get_cull_indices = function(cull_params, raccoon_dead_alive_vect,
     } else if(cull_params$strategy == "age"){
 
         # Only choosing alive juveniles (< 12 months)
-        pop_inds = which((age_vect < 12) & (!is.na(age_vect)))
+        pop_inds = which((age_vect < cull_params$age) & (!is.na(age_vect)))
       
     } else if(cull_params$strategy == "human"){
 
@@ -180,6 +181,15 @@ get_birth_control_indices = function(birth_control_params, repro_able_vect){
     # birth_control_params : list, parameters for...TODO
     # repro_able_vect : vector, containing 0 (can't reproduce), 
     #                    1 (can reproduce) or NA (dead)
+    #
+    # Returns
+    # -------
+    # indices of raccoons to give birth control too
+
+    # No culling if birth control is null
+    if(is.null(birth_control_params)){ 
+        return(integer(0))
+    }
 
     if(birth_control_params$strategy == "random"){
 
@@ -780,13 +790,11 @@ get_simulation_parameters = function(...){
 
     ## Raccoon parameters
     INIT_NUM_RACCOONS = 500
-    #DEATH_PROB = 22 # Natural death
     DEATH_THRESHOLD = 22 #22 # beta in death_probability fxn
     PATHOGENICITY = -4.2 # alpha in death_probability fxn
 
     # Probability of dying as baby. 
     BABY_DEATH = 1 - (.52^(1/7))^4 # From data at age 0 in fit_param.R
-    INTRINSIC_DEATH_RATE = 0.8512 # abs(slope) from fit_param.R.  WE MAY WANT TO REMOVE
     RANDOM_DEATH_PROB = 0.01 # Lower bound to death prob
     OLD_DEATH = (1 / (20 * 12)^2) # Above 20 years old the raccoon dies
     AGE_EGG_RESISTANCE = 4 # Age above which raccoons no longer pick up eggs
@@ -813,8 +821,6 @@ get_simulation_parameters = function(...){
     BETA = BIRTH_RATE / K_CAPACITY # From Encylopedia of Theoretical Ecology, pg. 634.
 
     # PARASITE PARAMETERS
-    # ENVIRONMENTAL_POOL = 0
-    # EGG_PRODUCTION_PER_WORM = 100 # eggs per month (note this is very wrong)
     ENCOUNTER_MEAN = 500 # Mean number of encountered eggs
     ENCOUNTER_K = 1 # Aggregation parameter of the NBD...
 
@@ -843,7 +849,6 @@ get_simulation_parameters = function(...){
                 DEATH_THRESHOLD=DEATH_THRESHOLD,
                 PATHOGENICITY=PATHOGENICITY,
                 BABY_DEATH=BABY_DEATH,
-                INTRINSIC_DEATH_RATE=INTRINSIC_DEATH_RATE,
                 RANDOM_DEATH_PROB=RANDOM_DEATH_PROB,
                 OLD_DEATH=OLD_DEATH,
                 AGE_EGG_RESISTANCE=AGE_EGG_RESISTANCE,
@@ -953,3 +958,201 @@ get_init_arrays = function(prms){
 
     return(init_arrays)
 }
+
+
+full_simulation = function(cull_params, birth_control_params, 
+            worm_control_params, management_time, 
+            prms, init_arrays, print_it=FALSE){
+    # Runs the full individual based simulation
+    #
+    # Parameters
+    # ----------
+    # cull_params : list or NULL, parameters for culling strategies. See cull_strategy
+    #    i.e. list(strategy="age", quota=5, overlap_threshold=0.5)
+    # management_time : int, time in simulation where management begins
+    # prms : list, simulation parameters
+    # init_arrays : initial arrays for holding results
+    # print_it : bool, if TRUE prints progress, otherwise False.
+
+
+    # Save the init arrays to the current environment
+    list2env(init_arrays, envir=environment())
+
+    # Loop through time steps
+    for(time in 2:(prms$TIME_STEPS + 1)){
+
+        if(print_it){
+            print(paste("Beginning step", time - 1, "of", prms$TIME_STEPS))
+        }
+
+
+        new_babies = 0
+        babies_at_this_time_vect = array(NA, dim=dim(raccoon_worm_array)[2])
+
+        # Calculate eggs remaining in each zone for this time step
+        eggs_remaining = get_cumulative_egg_load(time - 1, eggproduction_array, 
+                                            prms$EGG_DECAY)
+
+        # If time is >= management time begin management
+        if(time >= management_time){
+
+            # TODO: Set this up so you don't comment
+            cull_indices = get_cull_indices(cull_params,
+                                         raccoon_dead_alive_array[time - 1, ],
+                                         age_array[time - 1, ], 
+                                         human_overlap_through_time[[time - 1]])
+
+            # Pick individuals for birth control
+            birth_control_indices = get_birth_control_indices(birth_control_params,
+                                                repro_able_vect)
+
+            # If picked for birth control, can't have babies
+            repro_able_vect[birth_control_indices] = 0 
+
+            tworm_control_params = NULL #worm_control_params 
+        } else{
+            birth_control_indices = integer(0) 
+            cull_indices = integer(0)
+            tworm_control_params = NULL 
+        }
+
+        # Loop through raccoons but only through raccoons that are alive
+        alive_inds = which(raccoon_dead_alive_array[time - 1, ] == 1)
+        dead_inds = which(raccoon_dead_alive_array[time - 1, ] == 0)
+
+        # Keep the dead raccoons dead
+        raccoon_worm_array[time, dead_inds] = NA
+        raccoon_dead_alive_array[time, dead_inds] = 0
+
+        for(rac in alive_inds){
+
+            # 1. Raccoon dies (Killing at the beginning of the time interval)
+
+            age_now = initial_age_vector[rac] +
+                        sum(raccoon_dead_alive_array[, rac], na.rm=T)
+
+            # Get overlap and raccoon zones
+            overlap_now = human_overlap_through_time[[time - 1]][rac]
+
+            # NOTE: We could speed this up by assigning raccoons discrete zones
+            zone_now = get_raccoon_zone(overlap_now, prms$ZONES)
+
+            alive_now = kill_my_raccoon(raccoon_worm_array[time - 1, rac],
+                                            age_now, overlap_now, 
+                                            prms$DEATH_THRESHOLD, prms$PATHOGENICITY,
+                                            prms$BABY_DEATH,
+                                            prms$RANDOM_DEATH_PROB, prms$OLD_DEATH,
+                                            cull=any(rac == cull_indices))
+
+            raccoon_dead_alive_array[time, rac] = alive_now
+
+            if(alive_now){ # Do this if raccoon is alive
+
+                # 1. Give birth if appropriate
+
+                age_array[time, rac] = age_now
+
+                tot_racs = sum(raccoon_dead_alive_array[time, ], na.rm=T)
+                new_babies_now = give_birth(age_now, time, tot_racs, 
+                                                        repro_able_vect[rac],
+                                                        prms$MONTH_AT_REPRO,
+                                                        prms$FIRST_REPRO_AGE,
+                                                        prms$LITTER_SIZE, prms$BETA)
+
+                # Add new babies to array to assign human contacts later
+                babies_at_this_time_vect[rac] = new_babies_now
+                new_babies = new_babies + new_babies_now
+
+                # 2. Kill old worms for each possible source of worms
+                got_bait = picked_up_bait(overlap_now, tworm_control_params)
+
+
+                previous_cohorts = infra_worm_array[[rac]][time - 1, 1:(time - 1)]
+                new_cohort = kill_raccoon_worms(previous_cohorts,
+                                                    prms$WORM_SURV_TRESH,
+                                                    prms$WORM_SURV_SLOPE,
+                                                    got_bait=got_bait)
+
+                # Assign previous cohort to current cohort
+                infra_worm_array[[rac]][time, 1:(time - 1)] = new_cohort
+
+                # 3. Pick up eggs or pick up rodents depending on age
+                if(age_now <= prms$AGE_EGG_RESISTANCE){ # Only pick up worms from eggs
+
+                    worms_acquired = pick_up_eggs(prms$ENCOUNTER_MEAN,
+                                            prms$ENCOUNTER_K,
+                                            prms$INFECTIVITY, prms$RESISTANCE,
+                                            eggs_remaining[zone_now],
+                                            raccoon_worm_array[time - 1, rac],
+                                            prms$EGG_CONTACT)
+
+
+                } else{ # Only pick up worms from rodent
+                    worms_acquired = pick_up_rodents(prms$MOUSE_WORM_MEAN,
+                                                     prms$RODENT_ENCOUNTER_PROB,
+                                                     prms$LARVAL_WORM_INFECTIVITY,
+                                                     eggs_remaining[zone_now],
+                                                     prms$EGG_CONTACT)
+
+                }
+
+                infra_worm_array[[rac]][time, time] = worms_acquired
+                raccoon_worm_array[time, rac] = sum(infra_worm_array[[rac]][time, ], na.rm=T)
+
+                # 4. Disperse if raccoon is 6
+                if(age_now == prms$DISPERSAL_AGE){
+                    human_vect[rac] = assign_human_contacts(1)
+                }
+
+            } else { # Raccoon dead and its worms die
+                raccoon_worm_array[time, rac] = NA
+                human_vect[rac] = NA
+                repro_able_vect[rac] = NA
+            }
+
+        }
+
+        # Update the various vectors if babies were born
+        if(new_babies > 0){
+
+            updated_arrays = update_arrays(time, new_babies, new_babies_vect,
+                                               initial_age_vector,
+                                               raccoon_dead_alive_array,
+                                               raccoon_worm_array,
+                                               age_array, infra_worm_array,
+                                               human_vect,
+                                               repro_able_vect,
+                                               babies_at_this_time_vect,
+                                               prms$TIME_STEPS)
+
+            new_babies_vect = updated_arrays$new_babies_vect
+            initial_age_vector = updated_arrays$initial_age_vector
+            age_array = updated_arrays$age_array
+            raccoon_dead_alive_array = updated_arrays$raccoon_dead_alive_array
+            raccoon_worm_array = updated_arrays$raccoon_worm_array
+            infra_worm_array = updated_arrays$infra_worm_array
+            human_vect = updated_arrays$human_vect
+            repro_able_vect = updated_arrays$repro_able_vect
+
+        }
+
+        # Save the new human risk array
+        human_overlap_through_time[[time]] = human_vect
+        eggproduction_array[time, ] = assign_egg_production(
+                                            raccoon_worm_array[time, ],
+                                            human_vect,
+                                            prms$ZONES)
+
+    }
+
+    return(list(new_babies_vect=new_babies_vect, 
+                initial_age_vector=initial_age_vector,
+                age_array=age_array,
+                raccoon_dead_alive_array=raccoon_dead_alive_array,
+                raccoon_worm_array=raccoon_worm_array,
+                infra_worm_array=infra_worm_array,
+                human_vect=human_vect,
+                repro_able_vect=repro_able_vect,
+                human_overlap_through_time=human_overlap_through_time,
+                eggproduction_array=eggproduction_array))
+} # End full simulation
